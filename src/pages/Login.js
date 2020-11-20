f69462aa2be1a97ccea68f71a71c8792e9860c68
sukru <sukrugundogdudatascience@gmail.com>
2020-11-20 04:46:39 +0300

    fixed login page styeled

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,81 +1,72 @@
 import React, {useState} from 'react';
 import {
   SafeAreaView,
-  Dimensions,
   View,
+  Alert,
   Text,
   Image,
   ScrollView,
   KeyboardAvoidingView,
-  Alert
 } from 'react-native';
 
 import {authStyle} from './styles';
 import {Input, Button} from '../components';
+import {resolveAuthError} from '../functions';
 import auth from '@react-native-firebase/auth';
-import { resolveAuthError } from "../functions"
 
 const Login = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  async function login(){
+  async function login() {
     // auth()
     //   .signInWithEmailAndPassword(email, password)
     //   .then(() => alert("OK"))
     //   .catch((err) => Alert.alert("ClarusChat", resolveAuthError(err.code)));
-
-      try {
-        if (email === '' || password === '') {
-          Alert.alert('ClarusChat', resolveAuthError('auth/null-value'));
-        } else {
-          await auth().signInWithEmailAndPassword(email, password);
-          alert("OK")
-        }
-      } catch (error) {
-        Alert.alert('ClarusChat', resolveAuthError(error.code));
-      }
+    try {
+      await auth().signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      Alert.alert('ClarusChat', resolveAuthError(error.code));
+    }
   }
 
   return (
     <SafeAreaView style={{flex: 1}}>
       <KeyboardAvoidingView style={{flex: 1, backgroundColor: '#cfd8dc'}}>
         <ScrollView contentContainerStyle={{flex: 1}}>
-
-          <View style={authStyle.container} >
-              <Image
-                style={authStyle.logo}
-                source={require("../assets/logo.jpeg")}
-              />
-              <Text style={authStyle.logoText}>ClarusChat</Text>
+          <View style={authStyle.container}>
+            <Image
+              style={authStyle.logo}
+              source={require('../assets/logo.jpeg')}
+            />
+            <Text style={authStyle.logoText}>ClarusChat</Text>
           </View>
-
-          <View style={{flex:1}} >
+          <View style={{flex: 1}}>
             <Input
               inputProps={{
-                placeholder: "Type your email address..",
-                keyboardType: "email-address"
+                placeholder: 'Type your email address..',
+                keyboardType: 'email-address',
               }}
-              onType={value => setEmail(value)}
+              onType={(value) => setEmail(value)}
             />
             <Input
               inputProps={{
-                placeholder: "Type your password..",
-                secureTextEntry: true
+                placeholder: 'Type your password..',
+                secureTextEntry: true,
               }}
-              onType={value => setPassword(value)}
+              onType={(value) => setPassword(value)}
+            />
+
+            <Button title="Sign In" onPress={() => login()} />
+            <Button
+              title="Sign Up"
+              noBorder
+              onPress={() => props.navigation.navigate('Sign')}
             />
           </View>
-
-          <Button title="Sign In"
-            onPress={() => login()}
-          />
-          <Button title="Sign Up" noBorder  onPress={() => props.navigation.navigate("Sign")} />
-        
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
-
 export {Login};
