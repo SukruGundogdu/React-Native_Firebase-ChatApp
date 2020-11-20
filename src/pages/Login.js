7aa0ea75531f5bd52d46e485e1013c9ea70e8b97
sukru <sukrugundogdudatascience@gmail.com>
2020-11-20 03:21:58 +0300

    Sign page created

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,7 +24,7 @@ const Login = (props) => {
                 style={authStyle.logo}
                 source={require("../assets/logo.jpeg")}
               />
-              <Text style={authStyle.logoText}>CLARUSCHAT</Text>
+              <Text style={authStyle.logoText}>ClarusChat</Text>
           </View>
 
           <View style={{flex:1}} >
@@ -43,7 +43,7 @@ const Login = (props) => {
           </View>
 
           <Button title="Sign In" />
-          <Button title="Sign Up" />
+          <Button title="Sign Up" noBorder  onPress={() => props.navigation.navigate("Sign")} />
         
         </ScrollView>
       </KeyboardAvoidingView>
